1b35001fb6c40f59bd4a4696ed8fff43ceaf493a
JCHubby971 <105220091+JCHubby971@users.noreply.github.com>
2024-11-25 10:26:27 -0400

    Add files via upload

---
+++ project-java-spring-boot/src/main/java/fr/eni/projectspringboot/ProjectJavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.projectspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectJavaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectJavaSpringBootApplication.class, args);
+	}
+
+}
---
+++ project-java-spring-boot/src/test/java/fr/eni/projectspringboot/ProjectJavaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.projectspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectJavaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
